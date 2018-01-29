facce198bee39872260a25af7615f9ddb8db0c18
MMxM <mmedeiros450@gmail.com>
2018-01-29 18:08:56 -0200

    testing the function ignotum_ptrace_memwrite

---
+++ test/test-ignotum_ptrace_memwrite.c
@@ -0,0 +1,37 @@
+#include <ignotum.h>
+#include <unistd.h>
+#include <sys/ptrace.h>
+#include <sys/types.h>
+#include <wait.h>
+
+static const char change_me[]="You cannot change me !!!";
+
+void test(void){
+	printf("(%p) before -> %s\n", change_me, change_me);
+
+	ptrace(PTRACE_TRACEME);
+	kill(getpid(), SIGSTOP);
+
+	printf("(%p) after  -> %s\n", change_me, change_me);
+}
+
+int main(void){
+
+	pid_t pid = fork();
+	if(pid == -1){
+		perror("fork()");
+	} else if(pid == 0){
+		test();
+		exit(0);
+	}
+
+	waitpid(pid, NULL, 0);
+	size_t ret = ignotum_ptrace_memwrite(pid, "Yes, i can", 11, (long)change_me);
+	ptrace(PTRACE_CONT, pid, 0L, 0L);
+	waitpid(pid, NULL, 0);
+
+	printf("bytes written: %zu\n", ret);
+
+    return 0;
+
+}
