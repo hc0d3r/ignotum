da31ae28f677b768739b6a04fa48b90debd0d97c
MMxM <mmedeiros450@gmail.com>
2018-01-31 20:42:28 -0200

    update ignotum_getmappedaddr test

--- test/ignotum_getmappedaddr.c
+++ test/ignotum_getmappedaddr.c
@@ -1,10 +1,14 @@
 #include <ignotum.h>
+#include <sys/mman.h>
+
 
 int main(void){
     pid_t target_pid;
     int map_fd;
 
     ignotum_mapped_addr_t *addrs = NULL, *i = NULL;
+
+	mmap((void *)0x13370000, 4096, PROT_READ|PROT_WRITE, MAP_FIXED|MAP_ANONYMOUS|MAP_PRIVATE, -1, 0L);
 
     target_pid = getpid();
 	ignotum_openmap( target_pid, &map_fd );
