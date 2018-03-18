ba2e0e68aeda344f4965b013acef072365464841
MMxM <mmedeiros450@gmail.com>
2018-03-17 22:22:56 -0300

    update ignotum_memsearch.c

--- test/ignotum_memsearch.c
+++ test/ignotum_memsearch.c
@@ -1,8 +1,7 @@
 #include <ignotum.h>
 
 int main(void){
-	pid_t target_pid;
-	int map_fd, mem_fd;
+	int mem_fd;
 	size_t j;
 
 	char test[]="abcdefghijklmnopq ------ leet --------";
@@ -11,18 +10,15 @@ int main(void){
 	ignotum_mapped_addr_t *addrs = NULL, *i = NULL;
 	ignotum_mem_search_t result = DEFAULT_IGNOTUM_MEMSEARCH;
 
-	target_pid = getpid();
-	ignotum_openmap( target_pid, &map_fd );
-
 	printf("char test[] = %p\n", test);
 
-	if( ignotum_getmappedaddr(map_fd, &addrs) ){
+	if( ignotum_getmappedaddr(0, &addrs) ){
 		for(i=addrs; i!=NULL; i=i->next){
 			if(i->pathname.ptr == NULL)
 				continue;
 
 			if(!strcmp("[stack]", i->pathname.ptr)){
-				ignotum_openmem(target_pid, &mem_fd, O_RDONLY, 0);
+				ignotum_openmem(getpid(), &mem_fd, O_RDONLY, 0);
 				ignotum_memsearch("leet", 4, mem_fd , i->range, &result);
 
 				if(result.len){
@@ -39,7 +35,6 @@ int main(void){
 
 	free_ignotum_mapped_addr_t(&addrs);
 	free_ignotum_mem_search(&result);
-	close(map_fd);
 
 	return 0;
 
