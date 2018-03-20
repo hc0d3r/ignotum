c63b0fbf4c64db4bb15ab8c21033fb99bd85a1a2
MMxM <mmedeiros450@gmail.com>
2018-03-19 22:24:33 -0300

    removing useless char

--- test/ignotum_memwrite.c
+++ test/ignotum_memwrite.c
@@ -7,7 +7,7 @@ int main(void){
 	printf("(%p) before -> %s\n", change_me, change_me);
 
 	mem_fd = ignotum_openmem( getpid(), O_RDWR );
-	ignotum_memwrite(mem_fd, (off_t)change_me, "Yes, i can\0", 11);
+	ignotum_memwrite(mem_fd, (off_t)change_me, "Yes, i can", 11);
 
 	printf("(%p) after  -> %s\n", change_me, change_me);
 
