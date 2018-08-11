3def994c2a6acc33512ed9bff5d9d9309bc471ea
MMxM <mmedeiros450@gmail.com>
2018-08-10 23:47:16 -0300

    fix test

--- test/ignotum_getmaplist.c
+++ test/ignotum_getmaplist.c
@@ -11,7 +11,7 @@ int main(void){
     mmap((void *)0x13370000, 4096, PROT_READ|PROT_WRITE, MAP_FIXED|MAP_ANONYMOUS|MAP_PRIVATE, -1, 0L);
 
     /* 0 will open /proc/self/maps */
-    if((nb = ignotum_getmaplist(16343, &addrs)) > 0){
+    if((nb = ignotum_getmaplist(0, &addrs)) > 0){
         printf("maps total --> %zd\n", nb);
         for(i=0; i<addrs.len; i++){
             printf("start-address-> %zx | end-address-> %zx\n", addrs.maps[i].start_addr, addrs.maps[i].end_addr);
