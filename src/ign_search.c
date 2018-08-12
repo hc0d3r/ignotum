82e32946269a3a4985dd6a57cb29db9bd66ba222
MMxM <mmedeiros450@gmail.com>
2018-08-12 19:20:40 -0300

    small optimization in ignotum_search

--- src/ign_search.c
+++ src/ign_search.c
@@ -1,13 +1,15 @@
 #include "ign_search.h"
 
 size_t ignotum_search(ignotum_search_t *out, off_t remote_addr, const void *haystack, size_t hlen, const void *needle, size_t nlen){
-    size_t i, j, k, ret = 0;
+    size_t i, j, k, diff, ret = 0;
 
     if(!hlen || nlen > hlen){
         goto end;
     }
 
-    for(i=0;i<hlen;i++){
+    diff = hlen-nlen;
+
+    for(i=0;i<=diff;i++){
         for(j=i, k=0; j<hlen && k<nlen && *(char *)(haystack+j) == *(char *)(needle+k); j++, k++){
             if(k == nlen-1){
                 out->addrs = realloc(out->addrs, sizeof(off_t)*(out->len+1));
